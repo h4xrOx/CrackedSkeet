197db19f19059e5235de281d2fe8d0e5fd44503e
h4xr0x.cc#1337 <65768277+h4xrOx@users.noreply.github.com>
2021-08-01 10:37:22 -0500

    Add files via upload

---
+++ RealSkeet_rdata.h
@@ -0,0 +1,26 @@
+struct stRealSkeet_rdata{
+	UCHAR m0[0x184];
+	CHAR m184_str[18];
+	UCHAR m196[0xE];
+	CHAR m1A4_str[15];
+	UCHAR m1B3[0xD];
+	CHAR m1C0_str[21];
+	UCHAR m1D5[0xB];
+	CHAR m1E0_str[12];
+	UCHAR m1EC[0x8];
+	CHAR m1F4_str[39];
+	UCHAR m21B[0x1];
+	CHAR m21C_str[59];
+	UCHAR m257[0x31];
+	CHAR m288_str[13];
+	UCHAR m295[0x5E];
+	CHAR m2F3_str[9];
+	UCHAR m2FC[0x30];
+	CHAR m32C_str[19];
+	UCHAR m33F[0x4D];
+	CHAR m38C_str[33];
+	UCHAR m3AD[0x3];
+	CHAR m3B0_str[24];
+	CHAR m3C8_str[16];
+	UCHAR m3D8[0x1C28];
+};
